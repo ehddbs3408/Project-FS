716f110df48c136c5dec672def3c38208036ac9c
KimDongYun <67079959+ehddbs3408@users.noreply.github.com>
2023-08-25 10:24:47 +0900

    Test UI

---
+++ Assets/01.Scripts/Data/ScenarioData.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class ScenarioData
+{
+    public int id;
+    public string name;
+    public string text;
+    public string spriteNum;
+    public int backgroundNum;
+    public int navigationNum;
+    public int easeNum;
+    public int interfaceNum;
+    public int choice;
+    public int next;
+}
--- Assets/01.Scripts/Manager/UIManager.cs
+++ Assets/01.Scripts/Manager/UIManager.cs
@@ -10,7 +10,10 @@ public class UIManager : MonoBehaviour
 
     public UIDocument document;
 
-    public TextAsset _dialogue;
+    public TextAsset _dialogue;        
+    
+    public List<ScenarioData> scenarioDatas = new List<ScenarioData>();
+
     private void Awake()
     {
         if(Instance != null)
@@ -19,9 +22,46 @@ public class UIManager : MonoBehaviour
         }
         Instance = this;
     }
-
-    public void log(string message)
+    private void Start()
     {
-        string[] data = _dialogue.text.Split('\n');
+        StartScenario("");
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A)) { LoadData(); }
+    }
+
+    public void StartScenario(string scenarioName)
+    {
+        string[] line = _dialogue.text.Split('\n');
+        line[0] = "";
+        foreach(string s in line)
+        {
+            if (s == "") continue;
+            string[] scenario = s.Split(",");
+
+            ScenarioData newData = new ScenarioData();
+            newData.id = int.Parse(scenario[0]);
+            newData.name = scenario[1];
+            newData.text = scenario[2];
+            newData.spriteNum = scenario[3];
+            newData.backgroundNum = int.Parse(scenario[4] == "" ? "0" : scenario[4]);
+            newData.navigationNum = int.Parse(scenario[5] == "" ? "0" : scenario[5]);
+            newData.easeNum = int.Parse(scenario[6] == "" ? "0" : scenario[6]);
+            newData.interfaceNum = int.Parse(scenario[7] == "" ? "0" : scenario[7]);
+            newData.choice = int.Parse(scenario[8] == "" ? "0" : scenario[8]);
+            newData.next = int.Parse(scenario[9] == "" ? "0" : scenario[9]);
+
+            scenarioDatas.Add(newData);
+        }
+    }
+
+    public void LoadData()
+    {
+        foreach(ScenarioData data in scenarioDatas)
+        {
+            Debug.Log($"{data.id} {data.name} {data.text} {data.spriteNum} {data.backgroundNum} {data.navigationNum} {data.easeNum} {data.interfaceNum} {data.choice} {data.next} ");
+        }
     }
 }
